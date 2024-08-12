a2cc5fe4711daf0c790346ac1ef95b04f0e0c1a6
fm-phibia <83271712+fm-phibia@users.noreply.github.com>
2024-08-12 05:00:30 +0000

    ✨ comment削除処理追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,6 +12,13 @@ class CommentsController < ApplicationController
         # article_pathヘルパーはArticleのshowアクションを呼び出してくれる
         redirect_to article_path(@article)
     end
+
+    def destroy
+        @article = Article.find(params[:article_id])
+        @comment = @article.comments.find(params[:id])
+        @comment.destroy
+        redirect_to article_path(@article), status: :see_other
+    end
     
     private
         def comment_params
